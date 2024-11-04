4b4425597dce614efe2f752ff30daeb12b2df224
mNemGig <70545925+ianjberdahl@users.noreply.github.com>
2024-11-03 21:30:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,10 +28,9 @@ sudo apt-get update && sudo apt full-upgrade -y
 
 # Installing i3 and other tools (can add more)
 echo "Installing i3 tile manager and some Goodies!!"
+sudo git clone https://github.com/xct/kali-clean.git /home/$(cat /tmp/who)/
+
 sudo apt-get install -y wget curl git thunar cmake 
-sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
-sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
-sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 sudo apt-get install -y cifs-utils netexec neo4j nfs-common krb5-user freerdp2-x11 ftp ffuf rlwrap exploitdb
 sudo npm install -g tldr
 sudo npm fund
