8a51606c0fb486d47c0b92dabef3b7a6f3d02eb5
mNemGig <70545925+ianjberdahl@users.noreply.github.com>
2024-11-05 15:30:45 -0600

    Update installNOi3.sh

--- installNOi3.sh
+++ installNOi3.sh
@@ -4,6 +4,7 @@
 
 
 whoami > /tmp/who
+echo "IF YOUR USING PARROT OS"
 echo "Ensure your /etc/apt/sources.list.d/parrot.list has the lines uncommented"
 echo "----"
 echo "deb https://deb.parrot.sh/parrot lory main contrib non-free non-free-firmware"
@@ -39,14 +40,14 @@ sudo npm install -g tldr
 # Github Repos and Things
 sudo wget -q https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.6/LaZagne.exe -P /usr/share/windows-resources/binaries
 sudo wget -q https://github.com/SnaffCon/Snaffler/releases/download/1.0.170/Snaffler.exe -P /usr/share/windows-resources/binaries
-sudo wget -q https://github.com/TheWover/donut/releases/download/v1.0/donut_v1.0.tar.gz -P /tmp/Install
-sudo mv /tmp/Install/donut_v1.0/donut /opt/programs && sudo chmod +x /opt/programs/donut
+sudo wget -q https://github.com/TheWover/donut/releases/download/v1.0/donut_v1.0.tar.gz -P /tmp/Install && tar xzf /tmp/Install/donut_v1.0.tar.gz
+sudo mv /tmp/Install/donut /opt/programs && sudo chmod +x /opt/programs/donut
 sudo git clone https://github.com/XiaoliChan/LDAPShell.git /opt/tools
-sudo ln -s /opt/tools/LDAPShell/ldapshell.py /opt/programs/ldapshell
+sudo ln -s /opt/tools/LDAPShell/ldapshell.py /opt/programs/
 sudo wget -q https://github.com/flozz/p0wny-shell/raw/refs/heads/master/shell.php -P /opt/tools
 sudo wget -q https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64 -P /opt/programs
 sudo mv /opt/programs/kerbrute_linux_amd64 /opt/programs/kerbrute && sudo chmod +x /opt/programs/kerbrute
-sudo wget -q https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.1/BloodHound-linux-x64.zip -P /opt/tools
+sudo wget -q https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.1/BloodHound-linux-x64.zip -P /tmp/Install && unzip /tmp/Install/BloodHound-linux-x64.zip
 sudo mv /tmp/Install/BloodHound-linux-x64 /opt/tools
 sudo ln -s /opt/tools/BloodHound-linux-x64/BloodHound /opt/programs/BloodHound
 sudo git clone https://github.com/urbanadventurer/username-anarchy.git /opt/tools
@@ -63,21 +64,13 @@ sudo wget -q https://github.com/nicocha30/ligolo-ng/releases/download/v0.6.2/lig
 sudo wget -q https://github.com/nicocha30/ligolo-ng/releases/download/v0.6.2/ligolo-ng_agent_0.6.2_windows_amd64.zip -P /opt/tools/ligolo/windows
 sudo wget -q https://github.com/nicocha30/ligolo-ng/releases/download/v0.6.2/ligolo-ng_proxy_0.6.2_windows_amd64.zip -P /opt/tools/ligolo/windows
 
-# Installing pipx
-echo "Installing pipx and breaking system packages to do it"
-python3 -m pip install --user pipx --break-system-packages
-pipx ensurepath
-
-# Installing via pipx
-pipx install bloodhound-python
-pipx install 'git+https://github.com/ScorpionesLabs/MSSqlPwner.git'
-
 # Nano Syntax Highlighting
 echo "include /usr/share/nano/*.nanorc" > ~/.nanorc
 
 # Add to Path
 echo "Adding Path"
-echo "export PATH=$PATH:/opt/programs" >> /home/$(cat /tmp/who)/.zshrc
+#echo "export PATH=$PATH:/opt/programs" >> /home/$(cat /tmp/who)/.zshrc
+echo "export PATH=$PATH:/opt/programs" >> /root/.zshrc #if running as root
 source /home/jaybit/.zshrc
 
 # Summary
